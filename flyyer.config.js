1febf63e4a083832614018202402ac58ebfd92d6
Patricio López Juri <lopezjuripatricio@gmail.com>
2021-09-13 22:26:59 -0300

    feat: MVP

--- flyyer.config.js
+++ flyyer.config.js
@@ -1,6 +1,7 @@
 // Created with create-flyyer-app@2.1.2
 
 const {config} = require('@flyyer/types');
+const {default: endent} = require('endent');
 require('dotenv').config();
 
 module.exports = config({
@@ -10,7 +11,17 @@ module.exports = config({
 
   // Optionals
   name: 'Angled',
-  description: 'Created with create-flyyer-app',
-  private: true, // Set to false to deploy publicly to https://flyyer.io/community
+  description: endent`
+    Template we use in our landing page.
+
+    No affiliation with Nintendo is intended, this is just an example.
+
+    Image credits to: [Photo by KoolShooters from Pexels](https://www.pexels.com/photo/two-young-women-having-fun-7142962)
+  `,
+  homepage: 'https://www.flyyer.io',
+  license: 'MIT',
+  repository: 'flyyer-marketplace-angled',
+  keywords: ['official', 'ecommerce'],
+  private: false, // Set to false to deploy publicly to https://flyyer.io/community
   sizes: ['THUMBNAIL', 'BANNER', 'SQUARE', 'STORY'] // Declare supported sizes
 });
--- templates/main.tsx
+++ templates/main.tsx
@@ -1,14 +1,14 @@
-import React, {useState} from 'react';
+import React from 'react';
 import clsx from 'clsx';
 import {Variable as V, Validator, Static} from '@flyyer/variables';
+import {goerr} from '@flyyer/goerr';
 import {TemplateProps} from '@flyyer/types';
-import {proxy} from '@flyyer/proxy';
 
 import '../styles/tailwind.css';
 
 import logo from 'url:../static/logo.svg';
 import background from '../static/background.jpeg';
-import alternative from '../static/alternative.jpeg';
+import nintendo from '../static/nintendo.png';
 
 import {Layer} from '../components/layers';
 
@@ -16,17 +16,24 @@ import {Layer} from '../components/layers';
  * Export to enable variables UI on Flyyer.io
  */
 export const schema = V.Object({
-  title: V.String({default: 'Created with React.js and Tailwind'}),
-  description: V.Optional(V.String()),
-  image: V.Image({
-    title: 'Product image URL'
-  }),
-  background: V.Image({
-    title: 'Background image URL',
-    examples: [alternative],
-    default: background
-  }),
-  logo: V.Image({default: logo})
+  title: V.Nullable(V.String()),
+  currency: V.Optional(
+    V.String({default: 'USD', examples: ['USD', 'EUR', 'CLP', 'RUB']})
+  ),
+  price: V.Nullable(V.Number({examples: ['299']})),
+  image: V.Nullable(
+    V.Image({
+      title: 'Product image URL',
+      examples: [nintendo]
+    })
+  ),
+  background: V.Optional(
+    V.Image({
+      title: 'Background image URL',
+      default: background
+    })
+  ),
+  logo: V.Nullable(V.Image({default: logo}))
 });
 type Variables = Static<typeof schema>;
 
@@ -37,7 +44,7 @@ export default function MainTemplate(props: TemplateProps<Variables>) {
   const {width, height, variables, agent, locale = 'en'} = props;
 
   const {
-    data: {title, description, image, logo},
+    data: {title, image, background, logo, currency, price},
     isValid,
     errors
   } = validator.parse(variables);
@@ -45,52 +52,102 @@ export default function MainTemplate(props: TemplateProps<Variables>) {
     console.error('[Flyyer Variables]:', errors);
   }
 
-  const [counter, setCounter] = useState(0);
+  const [formatter, formatterError] = goerr(
+    () =>
+      new Intl.NumberFormat(locale, {
+        style: 'currency',
+        currency,
+        currencyDisplay: 'narrowSymbol',
+        maximumFractionDigits: 0 // 2
+      })
+  );
+  if (formatterError) {
+    console.warn(
+      '[Flyyer Template] Error creating Intl.NumberFormat: %o',
+      formatterError
+    );
+  }
 
   return (
-    <Layer className={clsx('relative grid grid-cols-12 grid-rows-12')}>
-      <div
-        className={clsx(
-          'col-span-12 row-span-12',
-          'banner:col-span-6 banner:row-span-12'
-        )}
-      >
-        <img
-          className="w-full h-full object-cover"
-          src={image}
-          crossOrigin="anonymous"
-        />
-      </div>
+    <Layer id="banner" className={clsx('bg-white')}>
+      <Layer className={clsx('grid grid-cols-12 grid-rows-12')}>
+        <aside className={clsx('col-span-6 row-span-12')}>
+          {background && (
+            <img
+              className="w-full h-full object-cover"
+              src={background}
+              crossOrigin="anonymous"
+            />
+          )}
+        </aside>
 
-      <header>
-        <button
-          onClick={() => {
-            setCounter((s) => s + 1);
-          }}
-        >
-          {counter}
-        </button>
-      </header>
-
-      {/* <Layer>
-        <img className="w-full h-full object-cover" src={proxy(image)} />
+        <aside className={clsx('col-span-6 row-span-10 p-3')}>
+          {image && (
+            <img
+              className="w-full h-full object-scale-down"
+              src={image}
+              crossOrigin="anonymous"
+            />
+          )}
+        </aside>
       </Layer>
-      <Layer className="bg-gradient-to-t from-black opacity-0 banner:opacity-60" />
-      <Layer className="flex flex-col justify-center items-center px-4 py-4 space-y-1 text-white text-center">
-        <img
-          src={proxy(logo)}
-          className="object-scale-down object-bottom w-full h-full banner:w-1/3 banner:h-auto banner:max-h-12 sq:max-h-16"
-        />
 
-        <h1 className="hidden banner:block text-xl sq:text-2xl font-bold tracking-tight leading-tight sq:leading-tight banner:line-clamp-3 sq:line-clamp-5">
-          {title}
-        </h1>
-        {description && (
-          <p className="hidden sq:block text-gray-50 tracking-tight sq:line-clamp-5">
-            {description}
-          </p>
-        )}
-      </Layer> */}
+      <Layer className={clsx('grid grid-cols-12 grid-rows-12')}>
+        <div
+          className={clsx(
+            'col-start-0 col-span-full row-start-10 row-span-3',
+            'flex flex-row justify-end items-center',
+            'px-3 py-1 space-x-1'
+          )}
+        >
+          {title && (
+            <div
+              className={clsx(
+                'min-w-0 flex-shrink',
+                'bg-yellow-300 pl-2 pr-1',
+                'border-b-2 border-r-2 border-yellow-400',
+                'filter drop-shadow-lg',
+                'transform -skew-x-12'
+              )}
+            >
+              <h1
+                className={clsx(
+                  'text-gray-900 text-base font-bold',
+                  'truncate',
+                  'transform skew-x-12'
+                )}
+              >
+                {title}
+              </h1>
+            </div>
+          )}
+
+          {formatter && IS_FINITE(price) && (
+            <div
+              className={clsx(
+                'flex-none',
+                'bg-gray-700 px-1',
+                'border-b-2 border-r-2 border-gray-800',
+                'filter drop-shadow-lg',
+                'transform -skew-x-12'
+              )}
+            >
+              <p
+                className={clsx(
+                  'text-white text-lg font-bold',
+                  'transform skew-x-12'
+                )}
+              >
+                {formatter.format(price)}
+              </p>
+            </div>
+          )}
+        </div>
+      </Layer>
     </Layer>
   );
+}
+
+function IS_FINITE(value: unknown): value is number {
+  return Number.isFinite(value);
 }
